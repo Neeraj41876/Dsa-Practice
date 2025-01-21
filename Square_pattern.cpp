d5b2f136a201baf74fc708f33984dc51241447a7
Neeraj41876 <neerajsingh24072003@gmail.com>
2025-01-21 23:12:30 +0530

    char

--- Square_pattern.cpp
+++ Square_pattern.cpp
@@ -15,17 +15,54 @@ using namespace std;
   //      
   //  }
   //  
-     int i=1;
-     while (i <=n) {
-       int  j = 1;
-        while (j <=n) {
-            cout <<j;
-            j++;
+   //  int i=1;
+   //  while (i <=n) {
+   //    int  j = 1;
+   //     while (j <=n) {
+   //         cout <<j;
+   //         j++;
+   //     }
+   //     cout << endl;
+   //     i++;
+   // }
+   // return 0;
+
+   // Reverse
+    // for (int i = n; i >=1; i--)
+    // {
+    //        for (int j = n; j >=1; j--)
+    //        {
+    //            cout<<j;
+    //        }
+    //        cout<<endl;
+    // }
+    //int i=1;
+    // while (n >=i) {
+    //     int  j = n;
+    //      while (j >=1) {
+    //            cout <<j;
+    //            j--;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //}
+
+    // using character
+
+for (int i = 1; i<=n; i++)
+    {
+        char ch='A';
+        for (int j = 1;j<=n; j++)
+        {
+            cout<<ch;
+            ch++;
+          
         }
-        cout << endl;
-        i++;
+            cout<<endl;
+   
+        
     }
-    return 0;
+
 }
 
   
