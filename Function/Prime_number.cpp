60fd5915a1120f449ea8f0cc9d5bbc603f2021ac
Neeraj41876 <neerajsingh24072003@gmail.com>
2025-01-23 22:28:32 +0530

    primeNumber

---
+++ Function/Prime_number.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std; 
+int prime(int n)
+{
+    bool isPrime=true;
+        for (int i = 2; i <n; i++)
+        {
+            if (n%i == 0)
+            {
+                isPrime=false;
+              
+
+            }
+        }
+
+        if(isPrime==true)
+        {
+            cout<<"Number is prime :"<<n<<endl;
+            
+        }
+        else
+        {
+            cout<<"Number is not  prime :"<<n<<endl;
+        }  
+            
+}
+int main()
+{
+    int number=0;
+     cout<<"Enter the number to check prime or not :";  
+     cin>>number;
+     prime(number);
+
+    
+return 0;
+}
